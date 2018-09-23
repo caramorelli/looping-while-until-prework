0faab38593b77bbb930df3fb2df99a634649463d
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 00:47:00 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -4,7 +4,7 @@ def using_until
     # 0.upto(levitation_force) { puts 'Wingardium Leviosa' }
     until levitation_force == 0 
       puts 'Wingardium Leviosa'
-      levitation_force--
+      levitation_force -= 1
     end 
 end
 
